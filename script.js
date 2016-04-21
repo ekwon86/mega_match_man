4647f6ee49ab28be1002561f6d58e04a8a3b2032
Eugene Kwon <ekwon86@gmail.com>
2016-04-20 21:06:13 -0700

    memory_match v0.5 - Eugene Kwon

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
+var first_card_clicked = null;
+var second_card_clicked = null;
+var total_possible_matched = 2;
+var match_counter = 0;
 $(document).ready(function() {
-    var first_card_clicked = null;
-    var second_card_clicked = null;
-    var total_possible_matched = 2;
-    var match_counter = 0;
 
-    function card_clicked(current) {
-        if (first_card_clicked == null) {
-            //this is the first card that was clicked of a set
-            first_card_clicked = current;
-            //show the card face
-            $('.back').click(function(){
-                $(this).hide();
-            });
-            //done with this function
-        } else {
-            //this is the second card that was clicked of a set
-            second_card_clicked = current;
-            //show the card face
-            //check if the first card and the second card are the same face / picture
-            //if they are, they match.  increment match_counter
-            //are all cards matched?
-            //if yes, player won game
-            //if not, continue playing game
-            //reset variables, we are going to match new cards next
-            //if they are not matched
-            //wait a short amount of time, then flip both cards back to normal
-            //reset variables, we are going to match new cards next
-        }
+    // $('.class').click(function(){
+    //     card_clicked(this);
+    // })
+
+});
+function card_clicked(current) {
+    if (first_card_clicked == null) {
+        //this is the first card that was clicked of a set
+        first_card_clicked = current;
+        //show the card face
+        $(first_card_clicked).find('.back').hide();
+
+        //done with this function
+    } else {
+        //this is the second card that was clicked of a set
+        second_card_clicked = current;
+        //show the card face
+        //check if the first card and the second card are the same face / picture
+        //if they are, they match.  increment match_counter
+        //are all cards matched?
+        //if yes, player won game
+        //if not, continue playing game
+        //reset variables, we are going to match new cards next
+        //if they are not matched
+        //wait a short amount of time, then flip both cards back to normal
+        //reset variables, we are going to match new cards next
     }
 }
