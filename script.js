da3097ffec2380e6430a32b1e1af436050d3ecca
Eugene Kwon <ekwon86@gmail.com>
2016-04-21 16:32:09 -0700

    memory_match v0.5 - Eugene Kwon

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-var first_card_clicked = null;
-var second_card_clicked = null;
-var total_possible_matched = 2;
+var first_card = null;
+var second_card = null;
+var total_possible_matches = 2;
 var match_counter = 0;
 
-$(document).ready(function() {
+//reset cards to null
+function resetCards() {
+    first_card = null;
+    second_card = null;
+}
+
+function card_clicked(current) {
+    if (first_card == null) {
+        first_card = current;
+        $(first_card).find('.back').hide();
+    } else {
+        second_card = current;
+        $(second_card).find('.back').hide();
+        //check if the first card and the second card are the same face / picture
+        //if they are, they match.  increment match_counter
+        if (first_card.find('frontimage').attr('src') == second_card.find('frontimage').attr('src')) {
+            match_counter++;
+            resetCards();
+        }
+        //are all cards matched?
+        //if yes, player won game
+        // if not, continue playing game
+        if (match_counter == total_possible_matches) {
+            alert('You won!');
+        }
+        //reset variables, we are going to match new cards next
+        //if they are not matched
+        //wait a short amount of time, then flip both cards back to normal
+        //reset variables, we are going to match new cards next        
+        else {
+        
+        }
+    }
+}
+
+
+$(document).ready(function () {
     // $('.class').click(function(){
     //     card_clicked(this);
     // })
 });
-
-function card_clicked(current) {
-    if (first_card_clicked == null) {
-        first_card_clicked = current;
-        $(first_card_clicked).find('.back').hide();
-    } else {
-        second_card_clicked = current;
-        $(second_card_clicked).find('.back').hide();
-        {
-            //check if the first card and the second card are the same face / picture
-            //if they are, they match.  increment match_counter
-            function checkCards() {
-                if ($(first_card_clicked).find('frontimage').attr('src') == $(second_card_clicked).find('frontimage').attr('src')) {
-                    return match_counter += 1;
-                }
-            }
-        }
-    }
-}
-                console.log(match_counter);
-        // //are all cards matched?
-        // //if yes, player won game
-        // // if not, continue playing game
-        // if (match_counter >= 9) {
-        //     alert('You won!');
-        // } else {
-        //
-        //
-        //
-        //
-        // //reset variables, we are going to match new cards next
-        // //if they are not matched
-        // //wait a short amount of time, then flip both cards back to normal
-        // //reset variables, we are going to match new cards next
-        //
