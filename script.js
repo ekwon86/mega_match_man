4c60d43afcd18008256e267cdb5050ea738f13bd
Eugene Kwon <ekwon86@gmail.com>
2016-04-22 16:51:06 -0700

    memory_match v1.0 - Eugene Kwon

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
+// want to eventually add health bar and functionality. when
+// user guesses wrong mega man's health will go down. successive
+// match will grant health to player and wrong guesses will
+// decrement health.
+
+// come up with way to insert game over song when player loses and
+// victory song when player wins!
+
+
 //global variables
 var first_card = null;
 var second_card = null;
-var total_possible_matches = 18 //$('.card').length / 2; //this is more dynamic
-var match_counter = 9;
+var total_possible_matches = 9;
+var match_counter = 0;
 var canClick = true;
 var card_flip_timer = null;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
+
+
 
 
 //reset cards to null
@@ -35,15 +50,19 @@ function card_clicked(current) {
         if ($(first_card).find('.frontimage').attr('src') == $(second_card).find('.frontimage').attr('src')) {
             $(first_card , second_card).addClass('matchedcards');
             $('.matchedcards').off("click");
-            //are all cards matched?
-            //if yes, player won game
-            // if not, continue playing game
+            //are all cards matched? if yes, player won game.if not, continue playing game
             resetCards();
+            attempts++;
+            console.log('total attempts equal to ' + attempts);
+            matches++;
+            console.log('total matches equal to ' + matches);
             match_counter++;
             if (match_counter == total_possible_matches) {
                 alert('You won!');
         }
         } else {
+            attempts++;
+            console.log('total attempts equal to ' + attempts);
             canClick = false;
             card_flip_timer = setTimeout(function() {
                 console.log('test - 1st card = ',first_card, "2nd card = ", second_card);
