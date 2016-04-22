d8df8b9cd983ddf60b0d42d069d7b1737f77a3d6
Eugene Kwon <ekwon86@gmail.com>
2016-04-22 14:06:40 -0700

    memory_match v0.5 - Eugene Kwon

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 //global variables
 var first_card = null;
 var second_card = null;
-var total_possible_matches = $('.card').length / 2;
-var match_counter = 0;
-var canClick= true;
+var total_possible_matches = 18 //$('.card').length / 2; //this is more dynamic
+var match_counter = 9;
+var canClick = true;
+var card_flip_timer = null;
+
 
 //reset cards to null
 function resetCards() {
     first_card = null;
     second_card = null;
 }
-function madeMatch (second_card) {
-    resetCards();
-    if (match_counter == total_possible_matches) {
-        alert('You won!');
-    }
 
 // ---- CARD FUNCITONS ---- //
 
@@ -22,42 +19,52 @@ function madeMatch (second_card) {
 function card_clicked(current) {
     if (canClick === false || $(current).hasClass('flipcard')) {
         return;
-        }
     }
+
+    console.log("card_clicked called - current is : ", current);
+
     $(current).addClass('flipcard');
 
-        if (first_card == null) {
-            first_card = current;
+    if (first_card == null) {
+        first_card = current;
+    } else {
+        second_card = current;
+        //check if the first card and the second card are the same face / picture
+        //if they are, they match.  increment match_counter
+        console.log($(first_card).find('.frontimage').attr('src'), $(second_card).find('.frontimage').attr('src'));
+        if ($(first_card).find('.frontimage').attr('src') == $(second_card).find('.frontimage').attr('src')) {
+            $(first_card , second_card).addClass('matchedcards');
+            $('.matchedcards').off("click");
+            resetCards();
+            match_counter++;
+            if (match_counter == total_possible_matches) {
+                alert('You won!');
+        }
         } else {
-            second_card = current;
-
-            //check if the first card and the second card are the same face / picture
-            //if they are, they match.  increment match_counter
-            if ($(first_card).find('frontimage').attr('src') == $(second_card).find('frontimage').attr('src')) {
-                $(first_card , second_card).addClass('matchedcards');
-                $('.matchedcards').off("click");
-                match_counter++;
+            // canClick = false;
+            card_flip_timer = setTimeout(function() {
+                card_flip_timer = null;
+                first_card.removeClass('flipcard');
+                second_card.removeClass('flipcard');
+                canClick = true;
                 resetCards();
-            }
-            //are all cards matched?
-            //if yes, player won game
-            // if not, continue playing game
-                if (match_counter == total_possible_matches) {
-                    alert('You won!');
-                }
-                    //reset variables, we are going to match new cards next
-                    //if they are not matched
-                    //wait a short amount of time, then flip both cards back to normal
-                    //reset variables, we are going to match new cards next
-                        else {
-                            canClick = false;
+            }, 2000);
+        }
+        //are all cards matched?
+        //if yes, player won game
+        // if not, continue playing game
 
+        //reset variables, we are going to match new cards next
+        //if they are not matched
+        //wait a short amount of time, then flip both cards back to normal
+        //reset variables, we are going to match new cards next
 
-                        }
-                }
-            }
+    }
+}
 
 //Keep doc ready here
 $(document).ready(function () {
-    $(".card").click (card_clicked);
+    $(".card").click(function(){
+        card_clicked(this);
+    });
 });
