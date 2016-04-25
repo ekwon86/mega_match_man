222ca3ebc64af54d19a4d5f43c332758dd785f33
Eugene Kwon <ekwon86@gmail.com>
2016-04-24 22:23:49 -0700

    memory_match v1.0 - Eugene Kwon

--- script.js
+++ script.js
@@ -19,16 +19,34 @@ var attempts = 0;
 var accuracy = 0;
 var games_played = 0;
 
-
-//accuracy function
-
-
-//display stats
-
 //reset cards to null
 function resetCards() {
     first_card = null;
     second_card = null;
+}
+//display_stats function
+function display_stats() {
+    $('.games-played .value').text(games_played);
+    $('.attempts .value').text(attempts);
+    $('.accuracy .value').text(accuracy + "%");
+}
+//accuracy function
+function get_accuracy() {
+    accuracy = Math.round((match_counter / attempts)) * 100;
+}
+//reset_stats function
+function reset_stats() {
+    attempts = 0;
+    accuracy = 0;
+    match_counter = 0;
+    display_stats();
+}
+//reset game function
+function reset() {
+    games_played++;
+    reset_stats();
+    display_stats();
+    $('.card').removeClass('flipcard');
 }
 
 // ---- CARD FUNCITONS ---- //
@@ -39,7 +57,7 @@ function card_clicked(current) {
         return;
     }
 
-    console.log("card_clicked called - current is : ", current);
+    //console.log("card_clicked called - current is : ", current);
 
     $(current).addClass('flipcard');
 
@@ -47,40 +65,49 @@ function card_clicked(current) {
         first_card = current;
     } else {
         second_card = current;
-        console.log($(first_card).find('.frontimage').attr('src'), $(second_card).find('.frontimage').attr('src'));
+        // console.log($(first_card).find('.frontimage').attr('src'), $(second_card).find('.frontimage').attr('src'));
         if ($(first_card).find('.frontimage').attr('src') == $(second_card).find('.frontimage').attr('src')) {
             $(first_card , second_card).addClass('matchedcards');
             $('.matchedcards').off("click");
             resetCards();
             attempts++;
-            console.log('total attempts equal to ' + attempts);
+            // console.log('total attempts equal to ' + attempts);
             matches++;
-            console.log('total matches equal to ' + matches);
+            // console.log('total matches equal to ' + matches);
+            get_accuracy();
             match_counter++;
             if (match_counter == total_possible_matches) {
                 alert('You won!');
         }
         } else {
             attempts++;
-            console.log('total attempts equal to ' + attempts);
+            // console.log('total attempts equal to ' + attempts);
             canClick = false;
             card_flip_timer = setTimeout(function() {
-                console.log('test - 1st card = ',first_card, "2nd card = ", second_card);
+                // console.log('test - 1st card = ',first_card, "2nd card = ", second_card);
                 $(first_card).removeClass('flipcard');
                 $(second_card).removeClass('flipcard');
                 card_flip_timer = null;
                 canClick = true;
                 resetCards();
+                get_accuracy();
+                display_stats();
             }, 1500);
         }
     }
 }
 
+
+
+
 //doc ready here
 $(document).ready(function () {
+    display_stats();
     $(".card").click(function(){
         card_clicked(this);
     });
 
-    // $(".accuracy").html(accuracy)
+    $('.reset').click(function() {
+        reset();
+    });
 });
