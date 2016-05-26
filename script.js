2973cac80d4d2b5380fa430f66a0c14dfc148bc2
Eugene Kwon <ekwon86@gmail.com>
2016-05-25 17:17:12 -0700

    hello

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-//TODO: want to eventually add health bar and functionality. when
-// user guesses wrong mega man's health will go down.
-// successive match will grant health to player and wrong guesses will
-
-
-/**** CARD CONSTRUCTOR ****/
-function card_constructor() {
+/*********************** CARD CONSTRUCTOR ***********************/
+function cards() {
     var first_card = null;
     var second_card = null;
     this.canClick = true;
@@ -26,25 +21,71 @@ function card_constructor() {
         second_card = null;
     };
 
-    /** CARD RANDOMIZER **/
+
     this.randomize_cards = function() {
-        var randomized_card_array = card_array.concat(card_array);
-        var slots = randomized_card_array.length;
+        var randomized_array = card_array.concat(card_array);
+        var randomized_array_length = randomized_array.length;
         var images_copy = [];
         
-        while (randomized_card_array > 0){
-            
+        for (var i = 0; i < randomized_array_length; i++){
+            var random_num = Math.floor((Math.random() * randomized_array.length));
+            images_copy.push(randomized_array.splice(random_num, 1));
+        }
+        
+        for (var j = 0; j < game.total_cards; j++){
+            var card = $('<div>').addClass('card');
+            var back = $('<div>').addClass('back').html('<img src=images/cardback2.jpg')
+            var front = $('<div>').addClass('front').html('<img src="' + images_copy[j] + '"></div>');
+            card.append(front);
+            card.append(back);
+            $('#game-area').append(card);
         }
     }
 }
 
-var total_possible_matches = 9;
-var match_counter = 0;
-var matches = 0;
-var attempts = 0;
-var accuracy = 0;
-var games_played = 0;
-var game_timer = null;
+
+/*********************** GAME CONSTRUCTOR ***********************/
+function game() {
+    this.match_counter = 0;
+    this.attempts = 0;
+    this.accuracy = 0;
+    this.games_played = 0;
+    this.game_timer = null;
+    this.total_cards = 18;
+    this.total_possible_matches = this.total_cards / 2;
+
+    /** INITIALIZE **/
+    this.init = function() {
+        cards.randomize_cards();
+        this.display_stats();
+    };
+
+    /** DISPLAY STATS **/
+    this.display_stats = function() {
+        $('.games-played .value').html(games_played);
+        $('.attempts .value').html(attempts);
+        $('.accuracy .value').html(accuracy + "%");
+    };
+
+    /** RESET STATS **/
+    this.reset = function() {
+        cards.reset_cards();
+        cards.canClick = true;
+        cards.card_flip_timer = null;
+        this.match_counter = 0;
+        this.matches = 0;
+        this.attempts = 0;
+        this.accuracy = 0;
+        this.game_timer = null;
+        this.games_played++;
+        display_stats();
+        $('.card').removeClass('flipcard');
+        $('.you-win').fadeOut('slow');
+        $('.you-lose').fadeOut('slow');
+    }
+
+}
+
 
 // ------------------------------ RANDOMIZE ---------------------------------- //
 // function randomize_cards() {
@@ -112,12 +153,6 @@ var game_timer = null;
 //     return Math.floor(Math.random() * max);
 // }
 
-// ----------------------------- DISPLAY STATS ------------------------------- //
-function display_stats() {
-    $('.games-played .value').html(games_played);
-    $('.attempts .value').html(attempts);
-    $('.accuracy .value').html(accuracy + "%");
-}
 
 // ----------------------------- GET ACCURACY ------------------------------- //
 function get_accuracy() {
@@ -125,27 +160,9 @@ function get_accuracy() {
     return accuracy;
 }
 
-// ----------------------------- RESET CARDS TO NULL -------------------------- //
-function reset_cards() {
-    first_card = null;
-    second_card = null;
-}
-
 // ----------------------------- RESET GAME -------------------------------- //
 function reset() {
-    reset_cards();
-    canClick = true;
-    card_flip_timer = null;
-    match_counter = 0;
-    matches = 0;
-    attempts = 0;
-    accuracy = 0;
-    game_timer = null;
-    games_played++;
-    display_stats();
-    $('.card').removeClass('flipcard');
-    $('.you-win').fadeOut('slow');
-    $('.you-lose').fadeOut('slow');
+
 }
 
 // --------------------------- CARD FUNCTIONS ------------------------------ //
@@ -194,26 +211,26 @@ function card_clicked(current) {
 }
 
 // ------------------------ CREATE GAME FUNCTION ------------------------- //
-function create_game() {
-    $('#start-game-button').hide();
-    if ($('#nighttime').is(':checked')) {
-        $('body').css('background-image', 'url(images/background.jpg)');
-        $('.back img').attr('src', 'images/cardback.png');
-        $('.card1 img').attr('src', 'images/flashman2.png');
-        $('.card2 img').attr('src', 'images/bubbleman2.png');
-        $('.card3 img').attr('src', 'images/airman2.png');
-        $('.card4 img').attr('src', 'images/metalman2.png');
-        $('.card5 img').attr('src', 'images/heatman2.png');
-        $('.card6 img').attr('src', 'images/woodman2.png');
-        $('.card7 img').attr('src', 'images/quickman2.png');
-        $('.card8 img').attr('src', 'images/crashman2.png');
-        $('.card9 img').attr('src', 'images/megaman2.png');
-    }
-    else if ($('#timed').is(':checked')) {
-        set_game_time();
-    }
-    $('#game-area, #stats-container').show();
-}
+// function create_game() {
+//     $('#start-game-button').hide();
+//     if ($('#nighttime').is(':checked')) {
+//         $('body').css('background-image', 'url(images/background.jpg)');
+//         $('.back img').attr('src', 'images/cardback.png');
+//         $('.card1 img').attr('src', 'images/flashman2.png');
+//         $('.card2 img').attr('src', 'images/bubbleman2.png');
+//         $('.card3 img').attr('src', 'images/airman2.png');
+//         $('.card4 img').attr('src', 'images/metalman2.png');
+//         $('.card5 img').attr('src', 'images/heatman2.png');
+//         $('.card6 img').attr('src', 'images/woodman2.png');
+//         $('.card7 img').attr('src', 'images/quickman2.png');
+//         $('.card8 img').attr('src', 'images/crashman2.png');
+//         $('.card9 img').attr('src', 'images/megaman2.png');
+//     }
+//     else if ($('#timed').is(':checked')) {
+//         set_game_time();
+//     }
+//     $('#game-area, #stats-container').show();
+// }
 
 // --------------------------- TIMER FUNCTION ------------------------------ //
 function set_game_time() {
@@ -235,7 +252,7 @@ function victory_music() {
 // --------------------------- SOUND FUNCTIONS ------------------------------ //
 
 $(document).ready(function() {
-    $('.you-win, .you-lose, #game-area, #stats-container').hide();
+    $('.you-win, .you-lose).hide();
 
     $('.close-modal').click(function(){
        // randomize_cards();
